54e0c97610747f78e8999601903cd0129ffc5329
dapengyou <ladyztt@sina.com>
2022-09-23 16:47:59 +0800

    自定义异常 和 先决条件函数

---
+++ src/study/ExceptionStudy.kt
@@ -0,0 +1,29 @@
+package study
+
+import java.lang.IllegalArgumentException
+import kotlin.Exception
+
+/**
+ * 自定义异常 和 先决条件函数
+ */
+fun main() {
+    try {
+        val info: String? = null
+        val value: Boolean = false
+        checkException(info)
+        //先决条件函数
+//        checkNotNull(info)
+//        requireNotNull(info)
+//        require(value)
+    } catch (e: Exception) {
+        println(e)
+    }
+
+}
+
+fun checkException(info: String?) {
+    info ?: throw  CustomException()
+}
+
+//自定义异常
+class CustomException : IllegalArgumentException("自定义异常")
